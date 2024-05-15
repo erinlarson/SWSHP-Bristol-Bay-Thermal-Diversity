d77c1b5f059e029c64510cf0d3fc882d575eff4f
Will Samuel <wtsamuel@alaska.edu>
2024-05-14 23:30:05 -0800

    Minor updates

--- fish/Fish bioenergetics.R
+++ fish/Fish bioenergetics.R
@@ -1,4 +1,4 @@
-#Primary authorw of this script: Erik Schoen and Will Samuel   
+#Primary authors of this script: Erik Schoen and Will Samuel   
 #Modeling Coho growth using bioenergetics
 #Project PI: Erin Larson, ACCS
 #Other collaborators/Co-PI's: Erik Schoen, Becky Shaftel, Sue Mauger, Daniel Schindler, Ben Meyer, Dustin Merrigan... 
@@ -17,15 +17,6 @@ set.seed(1)
 rm(list=ls(all=TRUE)) # clear the working environment
 #setwd("~/Development/SWSHP-Bristol-Bay-Thermal-Diversity")
 
-
-#Generate a dataframe with these columns: (drainage, site, year, date, and mean daily temp) that I can use as an input for bioenergetics simulations. This needs to be on a daily timestep, so interpolate temperatures between days if there are any gaps.
-#For the Nushagak drainage only, generate another dataframe with multiple simulations for each site year, allowing fish to either stay put or move downriver to the next site within the river network progressively throughout the growing season. For simplicity, Id say we could allow fish to move or stay put at the end of each week, and then stay put at that site for the next 7 days. 
-
-
-
-# 1) Daily time series for all the sites (drainage/HUC8, Site, Year, Date, Mean daily temp)
-# 2) Spatial heirarchy in the NUSH    Mulchatna, upper nush, lower nush, nush bay 
-# 3) Arc GIS map: Color points with fish growth potential for each site on the points on the map, compare a warm year and a cold year. 
 
 
 
@@ -53,6 +44,10 @@ write_csv(unique_sites, "./fish/newdata/unique_temp_sites.csv")
 
 
 # For the Nushagak River, generate an occupancy matrix --------------------
+#For the Nushagak drainage only, generate another dataframe with multiple simulations for each site year, allowing fish to either stay put or move downriver to the next site within the river network progressively throughout the growing season. For simplicity, Id say we could allow fish to move or stay put at the end of each week, and then stay put at that site for the next 7 days. 
+
+
+### Still in progress ###
 
 #Nushagak daily temperatures
 nush <- full.temp %>% 
@@ -62,33 +57,6 @@ unique(nush$SiteID)
 
 #Labeling the site numbers from most downstream (1) to furthest upstream)
 #because this is a dendritic stream network (as opposed to a linear path), this is an approximation
-nush <- nush %>% 
-  mutate(
-         site_num = ifelse(HUC8 = "UW_Aleknagik Squaw Creek", 1, ""),
-         site_num = ifelse(HUC8 = "cik_Squaw Creek", 2, site_num),
-         site_num = ifelse(HUC8 = "USGS_15302812", 3, site_num),
-         site_num = ifelse(HUC8 = "cik_Panaruqak Creek", 4, site_num),
-         site_num = ifelse(HUC8 = "cik_Tunravik Creek", 5, site_num),
-         site_num = ifelse(HUC8 = "cik_Sivanguq Creek", 6, site_num),
-         site_num = ifelse(HUC8 = "cik_Napotoli Creek", 7, site_num),
-         site_num = ifelse(HUC8 = "usgs_15302250", 8, site_num),
-         site_num = ifelse(HUC8 = "usgs_15301500", 9, site_num),
-         site_num = ifelse(HUC8 = "usgs_15302300", 10, site_num),
-         site_num = ifelse(HUC8 = "usgs_15302320", 11, site_num),
-         site_num = ifelse(HUC8 = "accs_mustu17", 12, site_num),
-         site_num = ifelse(HUC8 = "usgs_15302250", 13, site_num),
-         site_num = ifelse(HUC8 = "accs_musfk01", 14, site_num),
-         site_num = ifelse(HUC8 = "accs_mutsk02", 15, site_num),
-         site_num = ifelse(HUC8 = "accs_mutsk09", 16, site_num),
-         site_num = ifelse(HUC8 = "usgs_15302200", 17, site_num),
-         site_num = ifelse(HUC8 = "accs_mussm15", 18, site_num),
-         site_num = ifelse(HUC8 = "accs_muekm23", 19, site_num),
-         site_num = ifelse(HUC8 = "accs_AKBB-040", 20, site_num),
-         site_num = ifelse(HUC8 = "accs_mubon10", 21, site_num),
-         site_num = ifelse(HUC8 = "accs_AKBB-011", 22, site_num),
-         site_num = ifelse(HUC8 = "accs_AKBB-020", 23, site_num),
-         )
-
 
 nush <- nush %>% 
   mutate(site_num = ifelse(SiteID == "UW_Aleknagik Squaw Creek", 1, 
@@ -114,31 +82,9 @@ nush <- nush %>%
                      ifelse(SiteID == "accs_mubon10", 21, 
                      ifelse(SiteID == "accs_AKBB-011", 22, 
                      ifelse(SiteID == "accs_AKBB-020", 23, NA)))))))))))))))))))))))) %>% 
-  mutate(site_upstream = site_num = ifelse(SiteID == "UW_Aleknagik Squaw Creek", 1, 
-                     ifelse(SiteID == "UW_Aleknagik Squaw Creek", 2, 
-                     ifelse(SiteID == "cik_Squaw Creek", 3, 
-                     ifelse(SiteID == "USGS_15302812", 4, 
-                     ifelse(SiteID == "cik_Panaruqak Creek", 5, 
-                     ifelse(SiteID == "cik_Tunravik Creek", 6, 
-                     ifelse(SiteID == "cik_Sivanguq Creek", 7, 
-                     ifelse(SiteID == "cik_Napotoli Creek", 8, 
-                     ifelse(SiteID == "usgs_15302250", 9, 
-                     ifelse(SiteID == "usgs_15301500", 10, 
-                     ifelse(SiteID == "usgs_15302300", 11, 
-                     ifelse(SiteID == "usgs_15302320", 12, 
-                     ifelse(SiteID == "accs_mustu17", 13, 
-                     ifelse(SiteID == "usgs_15302250", 14, 
-                     ifelse(SiteID == "accs_musfk01", 15, 
-                     ifelse(SiteID == "accs_mutsk02", 16, 
-                     ifelse(SiteID == "accs_mutsk09", 17, 
-                     ifelse(SiteID == "usgs_15302200", 18, 
-                     ifelse(SiteID == "accs_mussm15", 19, 
-                     ifelse(SiteID == "accs_muekm23", 20, 
-                     ifelse(SiteID == "accs_AKBB-040", 21, 
-                     ifelse(SiteID == "accs_mubon10", 22, 
-                     ifelse(SiteID == "accs_AKBB-011", 23, 
-                     ifelse(SiteID == "accs_AKBB-020", NA, NA))))))))))))))))))))))))
-
+  mutate(site_upstream = ifelse(site_num == 23, NA, site_num+1), 
+         site_downstream = ifelse(site_num > 1, site_num-1, NA))
+#Hmmm these site numbers are missing some of the USGS sites... 
 
 str(nush)
 
